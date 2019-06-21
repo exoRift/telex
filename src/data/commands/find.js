d5926d93c561d690d9642d4548acb1475545d0e1
mets11rap <20345400+mets11rap@users.noreply.github.com>
2019-06-21 10:49:04 -0400

    Add find command

---
+++ src/data/commands/find.js
@@ -0,0 +1,41 @@
+const {
+  Command
+} = require('cyclone-engine')
+
+const {
+  statusEmojis
+} = require('../utils.js')
+
+const data = {
+  name: 'find',
+  desc: 'Return a user\'s ID (useful for mentioning across guilds)',
+  options: {
+    args: [{ name: 'username', mand: true }, { name: 'discriminator', mand: true }]
+  },
+  action: ({ client, args: [username, discriminator] }) => {
+    const guildMembers = client.guilds.map((g) => g.members)
+    for (const members of guildMembers) {
+      const user = members.find((m) => m.username === username && m.discriminator === discriminator)
+      if (user) {
+        return {
+          embed: {
+            author: {
+              name: user.id,
+              icon_url: user.avatarURL
+            },
+            title: `Status: ${statusEmojis[user.game && user.game.type === 1 ? 'streaming' : user.status]}`,
+            description: '<@ID> to mention the user',
+            color: 6422766
+          }
+        }
+      }
+    }
+    return {
+      embed: {
+        title: 'Could not find user.'
+      }
+    }
+  }
+}
+
+module.exports = new Command(data)
