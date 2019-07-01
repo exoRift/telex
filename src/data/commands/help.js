41e8e85a47ef16cefe88d27eb995ff4cc8462091
mets11rap <20345400+mets11rap@users.noreply.github.com>
2019-07-01 15:22:07 -0400

    Help command now utilizes Cyclone built in help menu builder

--- src/data/commands/help.js
+++ src/data/commands/help.js
@@ -1,18 +1,20 @@
+const pageRegex = /page (\d+?)/
+
 const { join } = require('path')
 
 const {
-  PREFIX,
   SUPPORT_SERVER
 } = process.env
 
 const {
   Command,
-  Await
+  Await,
+  ReactCommand,
+  ReactInterface
 } = require('cyclone-engine')
 
 const {
-  prefixIcon,
-  helpIcon
+  prefixIcon
 } = require('../utils.js').links
 
 const data = {
@@ -21,43 +23,43 @@ const data = {
   options: {
     args: [{ name: 'page #' }]
   },
-  action: function ({ client, commands, replacers, args: [num = 0] }) {
-    const fields = ['']
+  action: async ({ agent, client, msg, args: [page] }) => {
     const pkg = require(join(process.cwd(), '/package.json'))
-    for (const command of [...commands.values()]) {
-      if (command.restricted) continue
-      const content = command.info
-      let index = fields.length - 1
-      if ((fields[index] + content).length > 1024) {
-        index++
-        fields[index] = ''
-      }
-      fields[index] += (fields[index].length ? '\n' : '') + content
-    }
-    fields.push('**Replacers:**\n*Inserts live data values into commands. `|REPLACERNAME|`*\n\n' + [...replacers.values()].reduce((a, e) => `${a}**${e.info}*\n`, ''))
-    const embed = {
-      title: '*[Click for support]* Made by mets11rap',
+
+    const helpMenuData = {
       description: `${client.user.username} is a bot that can allow you to set up rooms that guilds can connect to and have chat across all connected guilds. Click [here](https://discordbots.org/bot/${client.user.id}) to add me to your server!\n\nOnce the bot has joined a room, simply type in the assigned channel to transmit your message. [Github](${pkg.repository.url.substring(4)})`,
-      url: 'https://discord.gg/' + SUPPORT_SERVER,
+      supportServerInviteCode: SUPPORT_SERVER,
       color: 33023,
-      footer: {
-        icon_url: prefixIcon,
-        text: `Prefix: "${PREFIX}" or mention | <> = Mandatory () = Optional`
-      },
-      thumbnail: {
-        url: client.user.dynamicAvatarURL('png')
-      },
-      author: {
-        name: `${client.user.username} ${pkg.version} Help`,
-        icon_url: helpIcon
-      },
-      fields: [
-        {
-          name: `Commands Page ${parseInt(num) || 1} out of ${fields.length}`,
-          value: fields[parseInt(num) - 1] || fields[0]
-        }
-      ]
+      prefixImage: prefixIcon,
+      version: pkg.version,
+      page
     }
+    const helpMenu = await agent.buildHelp(helpMenuData)
+
+    const buttons = [
+      new ReactCommand({
+        emoji: '↩',
+        action: async ({ msg }) => {
+          const page = parseInt(msg.embeds[0].fields[0].name.match(pageRegex)[1])
+
+          helpMenuData.page = page - 2
+          const helpMenu = await agent.buildHelp(helpMenuData)
+
+          msg.edit({ embed: helpMenu })
+        }
+      }),
+      new ReactCommand({
+        emoji: '↪',
+        action: async ({ msg }) => {
+          const page = parseInt(msg.embeds[0].fields[0].name.match(pageRegex)[1])
+
+          helpMenuData.page = page + 1
+          const helpMenu = await agent.buildHelp(helpMenuData)
+
+          msg.edit({ embed: helpMenu })
+        }
+      })
+    ]
 
     const wait = new Await({
       time: 15000,
@@ -66,20 +68,21 @@ const data = {
         args: [{ name: 'page #' }],
         refreshOnUse: true
       },
-      action: ({ msg, args: [num], lastResponse }) => {
-        embed.fields = [
-          {
-            name: `Commands Page ${parseInt(num) || 1} out of ${fields.length}`,
-            value: fields[parseInt(num) - 1] || fields[0]
-          }
-        ]
-        lastResponse.edit({ embed })
+      action: async ({ msg, args: [page], lastResponse }) => {
+        helpMenuData.page = page
+        const helpMenu = await agent.buildHelp(helpMenuData)
+        lastResponse.edit({ embed: helpMenu })
       }
     })
 
     return {
-      embed,
-      wait
+      embed: helpMenu,
+      wait,
+      reactInterface: new ReactInterface({
+        buttons,
+        restricted: true,
+        designatedUsers: msg.author.id
+      })
     }
   }
 }
