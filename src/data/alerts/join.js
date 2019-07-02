aaba22644b79f2ecc19c06701953b4e7bb0eed2c
Arthur Neuman <20345400+exoRift@users.noreply.github.com>
2019-07-02 08:37:55 -0400

    Bind transmit to agent

--- src/data/alerts/join.js
+++ src/data/alerts/join.js
@@ -1,9 +1,11 @@
 module.exports = ({ guildName, guildsInRoom }) => {
   return {
-    title: `__${guildName}__ has joined the room!`,
-    description: guildsInRoom >= 3
-      ? 'ATTENTION: The recommended number of guilds per room is 3. The more guilds you have, the slower messages will pass through due to ratelimiting.'
-      : '',
-    color: 65280
+    embed: {
+      title: `__${guildName}__ has joined the room!`,
+      description: guildsInRoom >= 3
+        ? 'ATTENTION: The recommended number of guilds per room is 3. The more guilds you have, the slower messages will pass through due to ratelimiting.'
+        : '',
+      color: 65280
+    }
   }
 }
