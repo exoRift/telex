a7060c267fa91406716128ab2d7b4538327a8e55
mets11rap <20345400+mets11rap@users.noreply.github.com>
2019-07-02 08:56:27 -0400

    Add announcement button

---
+++ src/data/roomButtons/owner/4_announce.js
@@ -0,0 +1,36 @@
+const {
+  Await
+} = require('cyclone-engine')
+
+const {
+  announce
+} = require('../../alerts')
+
+const data = {
+  name: 'Announce',
+  emoji: '📣',
+  action: ({ agent, msg, knex }) => {
+    return {
+      content: 'Type your announcement (Cancels in 10 seconds):',
+      wait: new Await({
+        options: {
+          timeout: 10000,
+          args: [{ name: 'announcement' }]
+        },
+        action: ({ args: [announcement] }) => {
+          return knex.get({
+            table: 'guilds',
+            columns: 'room',
+            where: {
+              id: msg.channel.guild.id
+            }
+          }).then(({ room }) => {
+            agent.transmit({ room, msg: announce({ guildName: msg.channel.guild.name, content: announcement }) })
+          })
+        }
+      })
+    }
+  }
+}
+
+module.exports = data
