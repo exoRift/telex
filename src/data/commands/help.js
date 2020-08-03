9bb970c661c1bd234cc3dba385f5b4a551cab4ce
Arthur Neuman <arthurbneuman@gmail.com>
2020-08-03 00:54:37 -0400

    Update help command to incorporate command guides

--- src/data/commands/help.js
+++ src/data/commands/help.js
@@ -9,10 +9,60 @@ const {
 
 const {
   Command,
-  Await,
-  ReactCommand,
-  ReactInterface
+  Await
 } = require('cyclone-engine')
+
+function pickMenu (agent, page = 1) {
+  if (!isNaN(parseInt(page))) { // Help menu
+    const pkg = require(join(process.cwd(), '/package.json'))
+
+    const helpMenuData = {
+      desc: `${agent.client.user.username} is a bot allows you to set up rooms that servers can connect to and chat across all connected servers. Click [here](https://discordbots.org/bot/${agent.client.user.id}) to add me to your server!\n\nOnce the bot has joined a room, simply type in the assigned channel to transmit your message. [Github](${pkg.repository.url.substring(4)})`,
+      serverCode: SUPPORT_SERVER,
+      color: 33023,
+      footerImage: 'https://raw.githubusercontent.com/exoRift/telex/master/assets/Prefix.png',
+      version: pkg.version
+    }
+
+    const {
+      embed,
+      options: {
+        reactInterface
+      }
+    } = agent.buildHelp(helpMenuData, page)
+    if (reactInterface) reactInterface._options.deleteAfterUse = true
+
+    const wait = new Await({
+      time: 15000,
+      options: {
+        check: (msg) => msg.content.startsWith(PREFIX + 'help'),
+        args: [{ name: 'page #' }],
+        refreshOnUse: true,
+        requirePrefix: true,
+        shouldShift: true
+      },
+      action: ({ args: [page], triggerResponse }) => {
+        const response = pickMenu(agent, page)
+
+        triggerResponse.edit(response)
+      }
+    })
+
+    return {
+      embed,
+      options: {
+        awaits: wait,
+        reactInterface
+      }
+    }
+  } else { // Command guide
+    const embed = agent.buildCommandGuide(page)
+
+    return {
+      embed
+    }
+  }
+}
 
 const data = {
   name: 'help',
@@ -20,69 +70,8 @@ const data = {
   options: {
     args: [{ name: 'page #' }]
   },
-  action: ({ agent, msg, args: [page] }) => {
-    const pkg = require(join(process.cwd(), '/package.json'))
-
-    const helpMenuData = {
-      desc: `${agent.client.user.username} is a bot allows you to set up rooms that servers can connect to and chat across all connected servers. Click [here](https://discordbots.org/bot/${agent.client.user.id}) to add me to your server!\n\nOnce the bot has joined a room, simply type in the assigned channel to transmit your message. [Github](${pkg.repository.url.substring(4)})`,
-      supportServerInviteCode: SUPPORT_SERVER,
-      color: 33023,
-      prefixImage: 'https://raw.githubusercontent.com/exoRift/telex/master/assets/Prefix.png',
-      version: pkg.version
-    }
-
-    const {
-      embed
-    } = agent.buildHelp(helpMenuData, page)
-
-    const buttons = [
-      new ReactCommand({
-        emoji: '↩',
-        action: ({ msg }) => {
-          page--
-          embed.fields = agent.buildHelp(helpMenuData, page).embed.fields
-
-          msg.edit({ embed })
-        }
-      }),
-      new ReactCommand({
-        emoji: '↪',
-        action: ({ msg }) => {
-          page++
-          embed.fields = agent.buildHelp(helpMenuData, page).embed.fields
-
-          msg.edit({ embed })
-        }
-      })
-    ]
-
-    const wait = new Await({
-      time: 15000,
-      options: {
-        check: (msg) => msg.content.startsWith('help'),
-        args: [{ name: 'page #' }],
-        refreshOnUse: true,
-        shiftCount: PREFIX.length
-      },
-      action: ({ msg, args: [page], triggerResponse }) => {
-        embed.fields = agent.buildHelp(helpMenuData, page).embed.fields
-
-        triggerResponse.edit({ embed })
-      }
-    })
-
-    return {
-      embed,
-      options: {
-        wait,
-        reactInterface: new ReactInterface({
-          buttons,
-          options: {
-            removeReaction: true
-          }
-        })
-      }
-    }
+  action: ({ agent, args: [page] }) => {
+    return pickMenu(agent, page)
   }
 }
 
