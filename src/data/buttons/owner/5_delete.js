f9b93b4af990f02641ef9e35a4975a16f66ea67d
Arthur Neuman <arthurbneuman@gmail.com>
2020-08-05 22:47:09 -0400

    Update delete button

--- src/data/buttons/owner/5_delete.js
+++ src/data/buttons/owner/5_delete.js
@@ -5,33 +5,32 @@ const {
 const data = {
   name: 'Delete',
   emoji: '❌',
-  action: ({ agent, msg }) => {
-    return agent.attachments.db('rooms')
+  action: async ({ agent, msg }) => {
+    const [roomData] = await agent.attachments.db('rooms')
       .select(['name', 'pass'])
       .where('owner', msg.channel.guild.id)
-      .then(([{ name, pass }]) => {
-        return {
-          content: `Are you sure you want to delete **${name}**? Please type your room password to confirm (Cancels in 10 seconds):`,
+
+    return {
+      content: `Are you sure you want to delete **${roomData.name}**? Please type your room password to confirm (Cancels in 10 seconds):`,
+      options: {
+        awaits: new Await({
           options: {
-            awaits: new Await({
-              options: {
-                timeout: 10000,
-                args: [{ name: 'password', mand: true }]
-              },
-              action: ({ agent, msg: response, args: [password], triggerResponse }) => {
-                if (password === pass) {
-                  triggerResponse.delete().catch((ignore) => ignore)
+            timeout: 10000,
+            args: [{ name: 'password', mand: true }]
+          },
+          action: async ({ msg: response, args: [pass], triggerResponse }) => {
+            if (pass === roomData.pass) {
+              await triggerResponse.delete().catch((ignore) => ignore)
 
-                  response.delete().catch((ignore) => ignore)
-                  msg.delete().catch((ignore) => ignore)
+              await response.delete().catch((ignore) => ignore)
+              await msg.delete().catch((ignore) => ignore)
 
-                  return agent.attachments.deleteRoom(name).then(() => `Room **${name}** deleted.`)
-                } else return '`Password incorrect.`'
-              }
-            })
+              return agent.attachments.deleteRoom(agent.client, agent.attachments.db, roomData.name).then(() => `Room **${roomData.name}** deleted`)
+            } else return '`Incorrect password`'
           }
-        }
-      })
+        })
+      }
+    }
   }
 }
 
